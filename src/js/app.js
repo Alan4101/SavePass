4d9106cc488987349fa6d455eece10dae316c0e5
Andrew Tolochkevych <andriy410tolochkevitch@gmail.com>
2020-12-23 18:11:17 +0200

    fix bugs

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,11 @@
 * modal      - processing and creation of modal windows
 * password   - data processing to send and receive from backend
 * utils      - middlewares
-* auth       - authorization file
 * */
 import '../style/index.scss'
 import './content.js'
-import { Password } from "./password";
 
 
-// document.getElementById('mainPage').addEventListener('click', function (e){
-//     e.stopPropagation()
-//     console.log('click')
-// })
 console.log('app.js')
 
 
